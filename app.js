456a4db4456350283c6ee2082bfd1574117155b9
KimInteger <kjs20151240@gmail.com>
2024-07-20 16:12:03 +0900

    test-Integer : test fetchAPI

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 const fs = require('fs');
 
+const qs = require('querystring')
+
 const server = http.createServer((req,res)=>{
     console.log(req.method);
     console.log(req.url);
@@ -15,25 +17,39 @@ const server = http.createServer((req,res)=>{
             res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
             res.end(data);
         });
-    } else if(req.method === 'POST' && req.url === '/test') {
-        async function fetchData() {
-            try {
-                const response = await fetch('http://localhost:8080/test');
-                if(!response.ok) {
-                    res.writeHead(response.status, {"Content-Type":"application/json"});
-                    res.end(JSON.stringify({error : "Error fetching data"}));
-                    return;
+    } else if(req.method === 'POST' && req.url === '/test') { 
+        let body = '';
+        req.on('data',(data)=>{
+            body += data.toString();
+        });
+        req.on('end',()=>{
+            const {id, password} = qs.parse(body);
+            async function fetchData() {
+                try {
+                    console.log(id,password);
+                    const response = await fetch('http://localhost:8080/test',{
+                        method : 'POST',
+                        headers : {
+                            'Content-Type' : 'application/json'
+                        },
+                        body : JSON.stringify({"id":id,"password" : password})
+                    });
+                    if(!response.ok) {
+                        res.writeHead(response.status, {"Content-Type":"application/json"});
+                        res.end(JSON.stringify({error : "Error fetching data"}));
+                        return;
+                    }
+                    const data = await response.json();
+                    res.writeHead(200,{"Content-Type":"application/json"});
+                    res.end(JSON.stringify(data));
+                } catch (error) {
+                    res.writeHead(500,{"Content-Type":"application/json"});
+                    res.end(JSON.stringify({error:"server error"}));
                 }
-                const data = await response.json();
-                res.writeHead(200,{"Content-Type":"application/json"});
-                res.end(JSON.stringify(data));
-            } catch (error) {
-                res.writeHead(500,{"Content-Type":"application/json"});
-                res.end(JSON.stringify({error:"server error"}));
             }
-        }
-
-        fetchData();
+    
+            fetchData();
+        });
     } else {
         res.writeHead(404,{"Content-Type":"text/html"});
         res.end('404 - Not Found');
